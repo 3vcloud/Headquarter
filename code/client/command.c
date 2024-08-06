3b1251ec1288f18850fe358690757327feacdf7b
Alex Macocian <amacocian@yahoo.com>
2024-08-06 21:15:40 +0200

    Make command.c skip additional params

--- code/client/command.c
+++ code/client/command.c
@@ -9,36 +9,36 @@ char** parsed_argv;
 
 void print_help(bool terminate)
 {
-    printf( "Usage: [options] <script>\n"
+    printf("Usage: [options] <script>\n"
 
-            "    --version                  print version and exist\n"
-            "    -c <config_file>           use config_file as configuration file\n"
-            "    -l <log_file>              Specify the name of the log file to write to\n"
-            "    -h, --help                 print this help\n"
-            "\n"
-            "    -account   <string>        Name of the account to use\n"
-            "    -email     <string>        Sets the client's email\n"
-            "    -password  <string>        Enable auto-loging. use with -email and -charname\n"
-            "    -character <string>        Sets the client's security question\n"
-            "    -status    <number>        Sets the login online status (0 = offline, 1 = online, 2 = busy, 3 = away)\n"
-            "    -authsrv                   Specify authserver IP to connect to\n"
-            "\n"
-            "    -oldauth                   The client will use Portal connection\n"
-            "\n"
-            "    -seed                      Specify the seed for pseudo random generation\n"
-            "    -v, --verbose              Enable debug logs\n"
-            "    -vv, --trace               Enable trace logs\n"
-            "    -file-game-version <path>  Provide a path to a file with the game version\n"
-            "\n"
-            "    -mapid                     Specify the map id you want to start in\n"
-            "    -maptype                   Specify the map type you want to start in\n"
-            "\n"
-        );
+        "    --version                  print version and exist\n"
+        "    -c <config_file>           use config_file as configuration file\n"
+        "    -l <log_file>              Specify the name of the log file to write to\n"
+        "    -h, --help                 print this help\n"
+        "\n"
+        "    -account   <string>        Name of the account to use\n"
+        "    -email     <string>        Sets the client's email\n"
+        "    -password  <string>        Enable auto-loging. use with -email and -charname\n"
+        "    -character <string>        Sets the client's security question\n"
+        "    -status    <number>        Sets the login online status (0 = offline, 1 = online, 2 = busy, 3 = away)\n"
+        "    -authsrv                   Specify authserver IP to connect to\n"
+        "\n"
+        "    -oldauth                   The client will use Portal connection\n"
+        "\n"
+        "    -seed                      Specify the seed for pseudo random generation\n"
+        "    -v, --verbose              Enable debug logs\n"
+        "    -vv, --trace               Enable trace logs\n"
+        "    -file-game-version <path>  Provide a path to a file with the game version\n"
+        "\n"
+        "    -mapid                     Specify the map id you want to start in\n"
+        "    -maptype                   Specify the map type you want to start in\n"
+        "\n"
+    );
 
     if (terminate) exit(0);
 }
 
-void check_for_more_arguments(int argc, const char **argv, int i, int nargs)
+void check_for_more_arguments(int argc, const char** argv, int i, int nargs)
 {
     if (argc <= i + nargs) {
         printf("Not enough arguments after '%s'\n", argv[i]);
@@ -46,7 +46,7 @@ void check_for_more_arguments(int argc, const char **argv, int i, int nargs)
     }
 }
 
-void parse_command_args(int argc, const char **argv)
+void parse_command_args(int argc, const char** argv)
 {
     // @Remark: Currently, if the format is not valid, for instance -email with no
     // arguments following, we will print the help and exit. Maybe we just want
@@ -57,58 +57,76 @@ void parse_command_args(int argc, const char **argv)
     options.online_status = 1;
 
     for (int i = 0; i < argc; i++) {
-        const char *arg = argv[i];
+        const char* arg = argv[i];
 
         if (!strcmp(arg, "-h") || !strcmp(arg, "--help")) {
             options.print_help = true;
-        } else if (!strcmp(arg, "--version")) {
+        }
+        else if (!strcmp(arg, "--version")) {
             options.print_version = true;
-        } else if (!strcmp(arg, "-v") || !strcmp(arg, "--verbose")) {
+        }
+        else if (!strcmp(arg, "-v") || !strcmp(arg, "--verbose")) {
             options.verbose = true;
-        } else if (!strcmp(arg, "-vv") || !strcmp(arg, "--trace")) {
+        }
+        else if (!strcmp(arg, "-vv") || !strcmp(arg, "--trace")) {
             options.trace = true;
-        } else if (!strcmp(arg, "-authsrv")) {
+        }
+        else if (!strcmp(arg, "-authsrv")) {
             check_for_more_arguments(argc, argv, i, 1);
             options.auth_srv = argv[++i];
-        } else if (!strcmp(arg, "-status")) {
+        }
+        else if (!strcmp(arg, "-status")) {
             check_for_more_arguments(argc, argv, i, 1);
             options.online_status = atoi(argv[++i]);
             if (options.online_status < 0 || options.online_status > 3) {
                 printf("Invalid -status\n");
                 print_help(true);
             }
-        } else if (!strcmp(arg, "-account")) {
+        }
+        else if (!strcmp(arg, "-account")) {
             check_for_more_arguments(argc, argv, i, 1);
             safe_strcpy(options.account, ARRAY_SIZE(options.account), argv[++i]);
-        } else if (!strcmp(arg, "-email")) {
+        }
+        else if (!strcmp(arg, "-email")) {
             check_for_more_arguments(argc, argv, i, 1);
 
             // @Remark: We need the email to be in lower cases, because
             // it is user to compute the static hash of the password.
             safe_strcpy(options.email, ARRAY_SIZE(options.email), argv[++i]);
             strlwc(options.email, ARRAY_SIZE(options.email));
-        } else if (!strcmp(arg, "-password")) {
+        }
+        else if (!strcmp(arg, "-password")) {
             check_for_more_arguments(argc, argv, i, 1);
             safe_strcpy(options.password, ARRAY_SIZE(options.password), argv[++i]);
-        } else if (!strcmp(arg, "-character")) {
+        }
+        else if (!strcmp(arg, "-character")) {
             check_for_more_arguments(argc, argv, i, 1);
             safe_strcpy(options.charname, ARRAY_SIZE(options.charname), argv[++i]);
-        } else if (!strcmp(arg, "-oldauth")) {
+        }
+        else if (!strcmp(arg, "-oldauth")) {
             options.newauth = false;
-        } else if (!strcmp(arg, "-mapid")) {
+        }
+        else if (!strcmp(arg, "-mapid")) {
             check_for_more_arguments(argc, argv, i, 1);
             options.opt_map_id.set = true;
             options.opt_map_id.map_id = atoi(argv[++i]);
-        } else if (!strcmp(arg, "-maptype")) {
+        }
+        else if (!strcmp(arg, "-maptype")) {
             options.opt_map_type.set = true;
             options.opt_map_type.map_type = atoi(argv[++i]);
-        } else if (!strcmp(arg, "-l")) {
+        }
+        else if (!strcmp(arg, "-l")) {
             check_for_more_arguments(argc, argv, i, 1);
             safe_strcpy(options.log_file_name, ARRAY_SIZE(options.log_file_name), argv[++i]);
-        } else if (!strcmp(arg, "-file-game-version")) {
+        }
+        else if (!strcmp(arg, "-file-game-version")) {
             check_for_more_arguments(argc, argv, i, 1);
             safe_strcpy(options.file_game_version, ARRAY_SIZE(options.file_game_version), argv[++i]);
-        } else {
+        }
+        else if (!strncmp(arg, "-", 1)) {
+            i++;
+        }
+        else {
             if (options.script) {
                 printf("You shouldn't specify more than one script to run\n");
                 print_help(true);
@@ -144,13 +162,13 @@ void parse_command_args(int argc, const char **argv)
         struct tm ts;
         // @Robustness: Deal with the error?
         time_localtime(&t, &ts);
-		if (strftime(timestamp, sizeof(timestamp), "%Y-%m-%d_%H-%M-%S", &ts) <= 0) {
-			abort();
-		}
+        if (strftime(timestamp, sizeof(timestamp), "%Y-%m-%d_%H-%M-%S", &ts) <= 0) {
+            abort();
+        }
 
-		if (snprintf(options.log_file_name, sizeof(options.log_file_name), "%s_%d.txt", timestamp, getpid()) == -1) {
-			abort();
-		}
+        if (snprintf(options.log_file_name, sizeof(options.log_file_name), "%s_%d.txt", timestamp, getpid()) == -1) {
+            abort();
+        }
     }
 
     if (!options.file_game_version[0]) {
@@ -160,9 +178,10 @@ void parse_command_args(int argc, const char **argv)
     else if (atoi(options.file_game_version) > 0) {
         // Version argument is numeric
         options.game_version = (uint32_t)atoi(options.file_game_version);
-    } else {
+    }
+    else {
         // Version argument may be a location on disk of a Gw.build file
-        FILE *file;
+        FILE* file;
         if ((file = fopen(options.file_game_version, "rb")) == NULL) {
             fprintf(stderr, "Failed to open '%s', err: %d\n", options.file_game_version, errno);
             abort();
